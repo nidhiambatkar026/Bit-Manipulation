a8ab938cc93e2ff6231784cbb6d39e0292e4f44b
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-04-05 19:15:13 +0530

    clearbit operation code added

---
+++ clearbit.java
@@ -0,0 +1,11 @@
+public class clearbit {
+    public static void main(String args[]){
+        int n = 5;
+        int pos =2;
+        int bitmask = 1<<pos;
+        int notbitmask = ~(bitmask);
+
+        int newNumber = notbitmask & n;
+        System.out.println(newNumber);
+    }
+}
